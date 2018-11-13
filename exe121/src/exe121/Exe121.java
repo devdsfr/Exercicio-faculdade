a89bc8c2023adb5aad67050f7dd832ff1e462175
devdsfr <devdsfr@gmail.com>
2018-11-13 08:18:14 -0200

    2 lista 13 11 2018

---
+++ exe121/src/exe121/Exe121.java
@@ -0,0 +1,21 @@
+package exe121;
+import java.util.Scanner;
+public class Exe121 {
+
+  
+    public static void main(String[] args) {
+        Scanner ent = new Scanner(System.in);
+        double count=0,idade,media,soma=0;
+        System.out.println("Infome sua idade");
+        idade=ent.nextDouble();
+        while(idade>0){
+        soma+=idade;
+        count ++;
+        System.out.println("Infome sua idade");
+        idade=ent.nextDouble();
+    }
+    media= soma/count;
+    System.out.println("A média das idade é:"+media);
+        
+    } 
+}
