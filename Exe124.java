acc87527101bbc413153c8c09e079d9e9de3eeba
devdsfr <devdsfr@gmail.com>
2018-11-13 11:01:44 -0200

    13112018
    news

---
+++ Exe124.java
@@ -0,0 +1,16 @@
+
+package exe.pkg124;
+
+/**cfsFDAD
+FDFD * @author Objeto-031
+ */
+public class Exe124 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
